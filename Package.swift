58cf12b26567a9d689bc8007c0417c517a122b86
Theo S <iosdeveloper@tgs.me.uk>
2026-02-17 09:34:35 +0000

    remove hyphen in paclage name

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "Data-Netlib",
+    name: "DataNetlib",
     platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
-        .library(name: "Data-Netlib", targets: ["Data-Netlib"])
+        .library(name: "libDataNetlib", targets: ["libDataNetlib"])
     ],
     dependencies: [],
     targets: [
@@ -13,7 +13,7 @@ let package = Package(
         // Resource‑only target
         // -----------------------------------------------------------------
         .target(
-            name: "Data-Netlib",
+            name: "libDataNetlib",
             resources: [
                 // `.copy` leaves files untouched (no asset compilation).
                 .copy("Resources") // <-- raw test data
