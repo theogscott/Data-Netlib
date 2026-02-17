2a5c6955f71dec3b69f2fb9c5973f38051ca6a36
Theo S <iosdeveloper@tgs.me.uk>
2026-02-17 10:57:54 +0000

    mke the testDarta public in the test bundle

---
+++ Sources/Clp/BundleHelper.swift
@@ -0,0 +1,14 @@
+//
+//  Untitled.swift
+//  DataNetlib
+//
+//  Created by Theo S   on 17/02/2026.
+//
+// DataNetlib/Sources/libDataNetlib/BundleHelper.swift
+import Foundation
+
+/// Public helper that gives callers access to the bundle that contains the test data.
+public enum LibDataNetlibResources {
+    public static var bundle: Bundle { .module }
+}
+
