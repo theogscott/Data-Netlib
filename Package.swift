d73288d26d3d58a9c11891f2aebc5793580206ed
Theo S <iosdeveloper@tgs.me.uk>
2026-02-22 05:55:12 +0000

    Spelling

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "DataNetlib",
     platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
-        .library(name: "dataNetlib", targets: ["dataNetlib"])
+        .library(name: "DataNetlib", targets: ["DataNetlib"])
     ],
     dependencies: [],
     targets: [
@@ -13,7 +13,7 @@ let package = Package(
         // Resource‑only target
         // -----------------------------------------------------------------
         .target(
-            name: "dataNetlib",
+            name: "DataNetlib",
             resources: [
                 // `.copy` leaves files untouched (no asset compilation).
                 .copy("Resources/Data/Netlib") // <-- raw test data
