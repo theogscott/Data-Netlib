f506ffcce1a817a5d08af808fe1e0691b9787be5
Theo S <iosdeveloper@tgs.me.uk>
2026-02-22 12:08:16 +0000

    Add back .gitignore item accidently deleted (by Xcode) Rename the public module

--- Sources/Clp/BundleHelper.swift
+++ Sources/Clp/BundleHelper.swift
@@ -4,11 +4,11 @@
 //
 //  Created by Theo S   on 17/02/2026.
 //
-// DataNetlib/Sources/libDataNetlib/BundleHelper.swift
+// 
 import Foundation
 
 /// Public helper that gives callers access to the bundle that contains the test data.
-public enum LibDataNetlibResources {
+public enum DataNetlib {
     public static var bundle: Bundle { .module }
 }
 
